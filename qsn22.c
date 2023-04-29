e236215b26bf5615f776c2c7d9b248963152fa12
nishanthkulal <nishanthkulal65462@gmail.com>
2023-04-29 23:23:15 +0530

    c problems

---
+++ qsn22.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*int main() {
+    int asciiValue;
+    printf("Enter an ASCII value: ");
+    scanf("%d", &asciiValue);
+    printf("The character for ASCII value %d is %c\n", asciiValue, (char) asciiValue);
+    return 0;
+}*/
+
+int main(){
+    int ascii;
+    scanf("%d",&ascii);
+    printf("%c",(char)ascii);
+}
